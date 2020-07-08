bf9b642281ad161d76786eee8b958effc20673f2
ritikvashistha <vashistharitik9@gmail.com>
2020-07-08 21:58:55 +0530

    Updates

---
+++ precache-manifest.a830edffdc36fd0bf66cde57c3423e0e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e8a3974d8d1320ce635bc7f1f7c0f2",
+    "url": "/reduxfire/index.html"
+  },
+  {
+    "revision": "d4ff343032d48a6557dc",
+    "url": "/reduxfire/static/js/2.af22d6c4.chunk.js"
+  },
+  {
+    "revision": "96ee6f6c8be10aa9908e",
+    "url": "/reduxfire/static/js/main.7ae3578f.chunk.js"
+  },
+  {
+    "revision": "881e583548e96877acaf",
+    "url": "/reduxfire/static/js/runtime-main.1e6f26d0.js"
+  }
+]);
